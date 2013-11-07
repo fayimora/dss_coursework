7b848d66e53df79c61232a6dc00cab00cbfefb54
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-11-07 08:53:09 +0000

    minor edits

--- BankServer.java
+++ BankServer.java
@@ -2,7 +2,7 @@ import java.rmi.*;
 
 public interface BankServer extends Remote
 {
-  public int deposit (int inc ) throws RemoteException;
-  public int withdraw (int dec ) throws RemoteException;
-  public int balance () throws RemoteException;
+  public int deposit(int inc ) throws RemoteException;
+  public int withdraw(int dec ) throws RemoteException;
+  public int balance() throws RemoteException;
 }
