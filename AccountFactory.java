f6cac4870df7ab4456de513c0a7aeff2c4373b49
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-11-26 07:51:50 -0500

    Return only positive numbers as account numbers

--- AccountFactory.java
+++ AccountFactory.java
@@ -8,7 +8,7 @@ class AccountFactory
   private HashMap<Integer, BankServer> accountsTable = new HashMap<Integer, BankServer>();
 
   public BankServer newAccount() throws RemoteException {
-    int accountNumber = new Random().nextInt();
+    int accountNumber = Math.abs(new Random().nextInt());
     BankServer s = new BankServerImpl(accountNumber);
     this.accountsTable.put(accountNumber, s);
     return s;
